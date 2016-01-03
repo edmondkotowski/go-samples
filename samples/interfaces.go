fa95570f8b3d1e390bec1780a7ed679cb10c7dca
Edmond Kotowski <ekotowski@gmail.com>
2016-01-03 14:42:22 -0800

    Added embedded types example to interfaces.go

--- samples/interfaces.go
+++ samples/interfaces.go
@@ -22,9 +22,24 @@ func (b *Baz) Run() string {
     return "Dynamic Baz"
 }
 
+// Embedded Types
+type UberFoo struct {
+    Bar  // Compostion
+    Name string
+}
+
+func (uf *UberFoo) Sprint() error {
+    fmt.Printf("Calling inner run %s with name %s\n", uf.Run(), uf.Name)
+
+    return nil
+}
+
 func main() {
     test_objects := []Foo{Bar{}, new(Baz)}
     for _, object := range test_objects {
         fmt.Println(object.Run())
     }
+
+    uberfoo := UberFoo{Name: "UberFoo"}
+    uberfoo.Sprint()
 }
